ea03d97c7bfec3f26fd63611348b63d52ba5d3b3
Tajudeen Yusuf <yusuftajudeenajibola@gmail.com>
2026-07-30 21:05:45 +0100

    added navbar component and it relevant code

---
+++ src/app/components/navbar/navbar.ts
@@ -0,0 +1,18 @@
+import { CommonModule } from '@angular/common';
+import { Component } from '@angular/core';
+import { RouterLink } from "@angular/router";
+import { NgIcon, provideIcons } from '@ng-icons/core';
+import { faBrandDribbble, faBrandBehance } from '@ng-icons/font-awesome/brands';
+
+@Component({
+  selector: 'app-navbar',
+  imports: [RouterLink, CommonModule, NgIcon],
+  templateUrl: './navbar.html',
+  styleUrl: './navbar.css',
+  viewProviders: [provideIcons({ faBrandDribbble, faBrandBehance })]
+})
+export class Navbar {
+
+  navLinks: string[] = ['Home', 'Features', 'Pricing', 'Blog' ];
+  navIcons: string[] =  ['faBrandDribbble', 'faBrandBehance'];
+}
