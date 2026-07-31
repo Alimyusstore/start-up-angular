cc9c8fe14fca219c39024ffa863a9ddee7e2bcde
Tajudeen Yusuf <yusuftajudeenajibola@gmail.com>
2026-07-31 14:14:21 +0100

    content section completed also changed the background color to black

--- src/app/app.ts
+++ src/app/app.ts
@@ -2,10 +2,11 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { Navbar } from './components/navbar/navbar';
 import { Hero } from "./components/hero/hero";
+import { Content } from "./components/content/content";
 
 @Component({
   selector: 'app-root',
-  imports: [Navbar, RouterOutlet, Hero],
+  imports: [Navbar, RouterOutlet, Hero, Content],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
