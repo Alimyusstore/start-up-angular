7d4fa323e7441370f6ba7421925e5b7edc97516f
Tajudeen Yusuf <yusuftajudeenajibola@gmail.com>
2026-08-04 17:02:34 +0100

    i comment out the template driven form and now write thre reactive form

--- src/app/components/forms/forms.ts
+++ src/app/components/forms/forms.ts
@@ -1,26 +1,45 @@
 import { Component } from '@angular/core';
-import { FormsModule } from '@angular/forms';
+import { FormBuilder, FormsModule, ReactiveFormsModule, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-forms',
-  imports: [FormsModule],
+  imports: [FormsModule, ReactiveFormsModule],
   templateUrl: './forms.html',
   styleUrl: './forms.css',
 })
 export class Forms {
   activeTab: string = 'signup';
-  email: string = '';
-  password: string = '';
-  name: string = '';
+  // email: string = '';
+  // password: string = '';
+  // name: string = '';
+forms;
+
+  constructor(private fb: FormBuilder) {
+  this.forms = this.fb.group({
+    name: ['name'],
+    email: ['', [Validators.required, Validators.email]],
+    password: ['', [Validators.required, Validators.minLength(6)]]
+  });
+  }
+  // private fb = new FormBuilder();
+
+
 
   setActiveTab(tab: string) {
     this.activeTab = tab;
   }
 
+
   submit(){
-    console.log({tab: this.activeTab, email: this.email, name: this.name, password: this.password})
-    this.email = '';
-    this.password = '';
-    this.name = '';
+    if (this.forms.invalid) {
+      this.forms.markAllAsTouched();
+      return;
+    }
+    console.log(this.forms.value);
+    this.forms.reset();
+    // console.log({tab: this.activeTab, email: this.email, name: this.name, password: this.password})
+    // this.email = '';
+    // this.password = '';
+    // this.name = '';
   }
 }
