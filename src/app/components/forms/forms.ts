468390a8ca4f4dfb952d07d62b49122377f5316d
Tajudeen Yusuf <yusuftajudeenajibola@gmail.com>
2026-08-04 17:46:11 +0100

    added a services and dummy api and console the result

--- src/app/components/forms/forms.ts
+++ src/app/components/forms/forms.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { FormBuilder, FormsModule, ReactiveFormsModule, Validators } from '@angular/forms';
+import { AuthService } from '../../services/auth-service';
 
 @Component({
   selector: 'app-forms',
@@ -14,7 +15,7 @@ export class Forms {
   // name: string = '';
 forms;
 
-  constructor(private fb: FormBuilder) {
+  constructor(private fb: FormBuilder, private authService: AuthService) {
   this.forms = this.fb.group({
     name: ['name'],
     email: ['', [Validators.required, Validators.email]],
@@ -31,12 +32,21 @@ forms;
 
 
   submit(){
-    if (this.forms.invalid) {
-      this.forms.markAllAsTouched();
-      return;
-    }
-    console.log(this.forms.value);
-    this.forms.reset();
+    // if (this.forms.invalid) {
+    //   this.forms.markAllAsTouched();
+    //   return;
+    // }
+    this.authService.signup(this.forms.value).subscribe({
+      next: (response) => {
+        console.log('Signup successful:', response, (this.forms.value));
+        this.forms.reset();
+      },
+      error: (error) => {
+        console.error('Signup failed:', error);
+      }
+    });
+    // console.log(this.forms.value);
+    // this.forms.reset();
     // console.log({tab: this.activeTab, email: this.email, name: this.name, password: this.password})
     // this.email = '';
     // this.password = '';
